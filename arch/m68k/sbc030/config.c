f972800977797530e4cdf5b9d582cd5528e09082
Piotr Binkowski <piotr.binkowski1@gmail.com>
2019-03-24 22:38:43 +0100

    sbc030: add mtd flash

--- arch/m68k/sbc030/config.c
+++ arch/m68k/sbc030/config.c
@@ -4,6 +4,9 @@
 #include <asm/sbc030hw.h>
 #include <linux/platform_device.h>
 #include <linux/spi/spi.h>
+#include <linux/spi/flash.h>
+#include <linux/mtd/partitions.h>
+#include <linux/mtd/physmap.h>
 
 extern void __init sbc030_init_IRQ(void);
 
@@ -67,6 +70,54 @@ static struct platform_device spi1_device = {
 	.resource = spi1_res,
 };
 
+static struct resource sst_flash_res = {
+	.start = SBC030_FLASH_BASE,
+	.end = SBC030_FLASH_BASE + SBC030_FLASH_SIZE - 1,
+	.flags = IORESOURCE_MEM,
+};
+
+static struct mtd_partition sst_flash_part = {
+	.name = "u-boot",
+	.size = 0x80000,
+	.offset = 0x0,
+};
+
+static struct physmap_flash_data sst_flash_data = {
+	.width = 1,
+	.parts = &sst_flash_part,
+	.nr_parts = 1,
+};
+
+static struct platform_device sst_flash_device = {
+	.name = "physmap-flash",
+	.id = 0,
+	.dev = {
+		.platform_data = &sst_flash_data,
+	},
+	.num_resources = 1,
+	.resource = &sst_flash_res,
+};
+
+static struct mtd_partition spi_flash_parts[] = {
+		{
+			.name = "kernel",
+			.size = 0x400000,
+			.offset = 0x0,
+		},
+		{
+			.name = "rootfs",
+			.size = 0xc00000,
+			.offset = 0x400000,
+		},
+	};
+
+static struct flash_platform_data spi_flash_data = {
+		.name = "w25q128",
+		.parts = spi_flash_parts,
+		.nr_parts = ARRAY_SIZE(spi_flash_parts),
+		.type = "w25q128",
+	};
+
 static struct spi_board_info sbc030_spi_info[] = {
 	{
 		.modalias = "enc28j60",
@@ -75,6 +126,14 @@ static struct spi_board_info sbc030_spi_info[] = {
 		.max_speed_hz = 20000000,
 		.bus_num = 1,
 		.chip_select = 0,
+	},
+	{
+		.modalias = "m25p80",
+		.mode = SPI_MODE_0,
+		.max_speed_hz = 20000000,
+		.bus_num = 0,
+		.chip_select = 0,
+		.platform_data = &spi_flash_data,
 	},
 };
 
@@ -152,6 +211,8 @@ int __init sbc030_platform_init(void)
 	platform_device_register(&spi0_device);
 
 	platform_device_register(&spi1_device);
+
+	platform_device_register(&sst_flash_device);
 
 	spi_register_board_info(sbc030_spi_info, ARRAY_SIZE(sbc030_spi_info));
 
