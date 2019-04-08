050233495e52598be084baf390ff209438eb92ad
Piotr Binkowski <piotr.binkowski1@gmail.com>
2019-04-08 22:38:10 +0200

    sbcon: handle attributes like the vgacon

--- drivers/video/console/sbcon.c
+++ drivers/video/console/sbcon.c
@@ -128,18 +128,23 @@ static int sbc_con_blank(struct vc_data *c, int blank, int mode_switch)
 
 static u8 sbc_con_build_attr(struct vc_data *c, u8 color, u8 intensity, u8 blink, u8 underline, u8 reverse, u8 italic)
 {
-	u8 attr;
+	u8 attr = color;
+
+	if(italic)
+		attr = (attr & 0xF0) | c->vc_itcolor;
+	else if(underline)
+		attr = (attr & 0xF0) | c->vc_ulcolor;
+	else if(intensity == 0)
+		attr = (attr & 0xF0) | c->vc_halfcolor;
 
 	if(reverse)
-		attr = (color & 0x07) << 4 | (color & 0x70) >> 4;
-	else
-		attr = color & 0x77;
+		attr = (attr & 0x88) | (((attr >> 4) | (attr << 4)) & 0x77);
 
 	if(blink)
-		attr |= 0x80;
+		attr ^= 0x80;
 
-	if(intensity)
-		attr |= 0x08;
+	if(intensity == 2)
+		attr ^= 0x08;
 
 	return attr;
 }
